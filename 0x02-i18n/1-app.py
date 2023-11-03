842f4b6e288383f385118e8b4acdb318c59fff3b
Patrick-052 <muiruripatrick052@gmail.com>
2023-11-03 20:32:59 +0300

    Basic babel configuration

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+""" Configuring my flask app to handle locale translation """
+
+from flask import Flask, render_template
+from flask_babel import Babel
+
+app = Flask(__name__)
+babel = Babel(app)
+
+
+class Config(object):
+    """ Configuring available languages in my app """
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+@app.route('/', strict_slashes=False)
+def hello_world():
+    """ Returns some text """
+    return render_template('1-index.html')
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000", debug=False)
