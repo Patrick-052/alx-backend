404fa19c90ce88f4d5ae4df2d5ab36b59a1cc6f9
Patrick-052 <muiruripatrick052@gmail.com>
2023-11-03 19:42:30 +0300

    Basic flask app

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -8,4 +8,8 @@ app = Flask(__name__)
 @app.route('/', strict_slashes=False)
 def hello_world():
     """ Returns some text """
-    render_template('0-index.html')
+    return render_template('0-index.html')
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000", debug=False)
